63246ba0d5d6bd7c38aabccd34bb34a4ed1cefd6
Archer <thivan.nms@gmail.com>
2024-05-20 10:16:50 +0700

    modified homepage to display a video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,49 +47,60 @@ const Page: NextPage = () => {
 
   return (
     <>
-      <Box
-        sx={{
-          backgroundImage: "url(/bg.jpg)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          height: "100vh",
-        }}
-      >
+      <Box sx={{ position: "relative", height: "100vh", width: "100vw", overflow: "hidden" }}>
+        <video
+          autoPlay
+          muted
+          loop
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            minWidth: "100%",
+            minHeight: "100%",
+            width: "auto",
+            height: "auto",
+            objectFit: "cover", // ensure the video was crop to fit if browser width was reduced
+            zIndex: 1,
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <source src="/sunset.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <Box
           sx={{
             height: "100vh",
+            width: "100vw",
             backgroundColor: "rgba(0, 0, 0, 0.5)",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 2,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            flexDirection: "row"
           }}
         >
-          <Box
+          <Typography
+            variant="h2"
             sx={{
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-              height: "100vh",
+              fontFamily: "Play Fair",
+              color: (theme) => theme.palette.common.white,
+              p: 4,
+              fontSize: { lg: "3.75rem", md: "3rem", sm: "2.5rem", xs: "2rem" },
+              mt: "35vh",
+              textAlign: "center"
             }}
           >
-            <Image src={"/logo-sm.png"} alt="logo" width={270} height={298} />
-            <Typography
-              variant="h2"
-              sx={{
-                fontFamily: "Play Fair",
-                color: (theme) => theme.palette.common.white,
-                p: 4,
-                fontSize: { lg: "3.75rem", md: 20, sm: 20, xs: 20 },
-              }}
-            >
-              Find Your Own Shine
-            </Typography>
-          </Box>
+            Find Your Own Shine
+          </Typography>
         </Box>
       </Box>
-      <ArticlePage
-        posts={posts}
-        title="Articles"
-      />
+      <Box sx={{ padding: 4 }}>
+        <ArticlePage posts={posts} title="Articles" />
+      </Box>
     </>
   );
 };
