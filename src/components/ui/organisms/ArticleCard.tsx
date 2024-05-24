dd989be5ee5bbd6dcefb79475dcd67be6bcc5514
Archer <thivan.nms@gmail.com>
2024-05-24 12:21:21 +0700

    allow author of the post to click edit to navigate to the editing page

--- src/components/ui/organisms/ArticleCard.tsx
+++ src/components/ui/organisms/ArticleCard.tsx
@@ -2,15 +2,25 @@
 
 import { formatDate } from "@/lib/utils";
 import { IPost } from "@/types";
-import { Avatar, Box, Paper, Stack, Typography } from "@mui/material";
-import Image from "next/image";
+import { Avatar, Box, Button, Paper, Stack, Typography } from "@mui/material";
 import { HeaderImage } from "./HeaderImage";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface SingleArticleProps {
   post: IPost;
 }
 
 const ArticleCard: React.FC<SingleArticleProps> = ({ post }) => {
+  // Check if user is the author of the post or not
+  const { data: session } = useSession();
+  const isAuthor = session?.user?.email === post.user.email;
+
+  // if author click edit, redirect them to the editing page
+  const router = useRouter();
+  const handleEdit = () => {
+    router.push("/");
+  }
   return (
     <Box>
       <Paper sx={{ position: "relative", height: "100vh", width: "100%" }}>
@@ -51,14 +61,22 @@ const ArticleCard: React.FC<SingleArticleProps> = ({ post }) => {
               Posted on: {formatDate(post.createdAt)}
             </Typography>
           </Box>
-          <Box display="flex" alignItems="center">
-            <Avatar
-              sx={{ height: 40, width: 40, marginRight: 1 }}
-              src={post.user.avatar}
-              alt={post.user.name}
-            />
-            <Typography variant="body2">{post.user.name}</Typography>
-          </Box>
+
+          {/* if user is author allow them to edit */}
+          {isAuthor ? (
+            <Button variant="contained" onClick={handleEdit}>
+              Edit
+            </Button>
+          ) : (
+            <Box display="flex" alignItems="center">
+              <Avatar
+                sx={{ height: 40, width: 40, marginRight: 1 }}
+                src={post.user.avatar}
+                alt={post.user.name}
+              />
+              <Typography variant="body2">{post.user.name}</Typography>
+            </Box>
+          )}          
         </Stack>
       </Paper>
     </Box>
