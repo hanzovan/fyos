69c69b70eb37e1f3a4abcfd14890f2ac76be7a55
Archer <thivan.nms@gmail.com>
2024-05-27 09:25:07 +0700

    modified post route handler to check if user is admin or author of the post in order to edit the post

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,8 +1,8 @@
 import { PostService } from "@/lib/services";
-import { NextApiRequest, NextApiResponse } from "next";
+import { checkUserRole } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req: NextRequest, res: NextApiResponse) {
+export async function PUT(req: NextRequest, res: NextResponse) {
   
   const id = req.nextUrl.pathname.split("/").pop();
 
@@ -15,6 +15,39 @@ export async function PUT(req: NextRequest, res: NextApiResponse) {
 
   try {
     const body = await req.json();
+
+    // get the user info
+    const userEncoded = req.headers.get("user");
+
+    if (!userEncoded) {
+      return NextResponse.json({ isError: true, message: "User information is missing" }, { status: 401 });
+    }
+
+    const user = JSON.parse(Buffer.from(userEncoded, "base64").toString("utf-8"));
+
+    // get the original post
+    const post = await PostService.getPostById(id)
+
+    if (!post) {
+      return NextResponse.json(
+        { isError: true, message: "Post not found" },
+        { status: 404 }
+      )
+    }
+
+    // check if user is the author of the original post or admin, if not reject request
+    const isAuthor = user.id === post.data?.user?.id;
+    const checkRole = checkUserRole(user.role);
+    const isAdmin = checkRole.isAdminRole;
+
+    if (!isAuthor && !isAdmin) {
+      return NextResponse.json(
+        { isError: true, message: "Not authorized" },
+        { status: 403 }
+      )
+    }
+    
+    // if user is admin or author, continue the request
     const result = await PostService.updatePost(id as string, body);
     if (result.isError) {
       return NextResponse.json(result, { status: 400 });
--- src/lib/services/post.ts
+++ src/lib/services/post.ts
@@ -1,4 +1,4 @@
-import { PostModel } from "@/database/models";
+import { PostModel, UserModel } from "@/database/models";
 import { PostBody, PostResponse } from "@/types";
 import { nanoid } from "nanoid";
 import slugify from "slugify";
@@ -61,6 +61,43 @@ const updatePost = async (id: string, body: PostBody): Promise<PostResponse> => 
     }
 }
 
-const PostService = { createNewPost, updatePost };
+const getPostById = async (id: string) => {
+    try {
+        const post = await PostModel.findById(id).populate({ path: "user", select: ["name", "email", "avatar", "_id"], model: UserModel }).lean().exec();
+        
+        if (!post) {
+            return {
+                isError: true,
+                data: null,
+                message: "Post not found",
+                status: 404
+            }
+        }
+
+        const data = {
+            ...post,
+            id: post._id?.toString(),
+            user: { ...post.user, id: post?.user?._id.toString() }
+        };
+
+        return {
+            isError: false,
+            data,
+            message: "Success",
+            status: 200
+        }
+    } catch (error) {
+        return {
+            isError: true,
+            data: null,
+            message: error instanceof Error
+                ? error.message
+                : "An unknown error occurred",
+            status: 500
+        }
+    }
+}
+
+const PostService = { createNewPost, updatePost, getPostById };
 
 export { PostService };
